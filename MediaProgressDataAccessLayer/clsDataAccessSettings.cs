4002e26b727420038f80d43bd3b97fc72386d6fa
Aym90B <aym90b@gmail.com>
2026-01-11 13:50:33 +0200

    New Config

--- MediaProgressDataAccessLayer/clsDataAccessSettings.cs
+++ MediaProgressDataAccessLayer/clsDataAccessSettings.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
 
 
 namespace MediaProgressDataAccessLayer
@@ -6,5 +8,21 @@ namespace MediaProgressDataAccessLayer
     static class clsDataAccessSettings
     {
         public static string ConnectionString = "Data Source=LT-4312\\SQLEXPRESS;Initial Catalog=MovieData;Integrated Security=True";
+    }
+}
+
+
+
+namespace MediaProgress.DAL
+{
+    public class DbConnection
+    {
+        // This reads the name "MediaDbConn" from whatever App.config is active
+        private static string _connectionString = ConfigurationManager.ConnectionStrings["MediaDbConn"].ConnectionString;
+
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
     }
 }
